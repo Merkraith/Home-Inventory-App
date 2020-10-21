2d8ce6713d043a7c3384f05a37c6ab6495b9cbd8
Mark Heil <mark.heil1122@gmail.com>
2020-10-21 00:21:02 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Home Inventory System
         </a>
       </header>
     </div>
